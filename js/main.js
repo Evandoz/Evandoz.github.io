7bd40e93c0ba6ed8b83295fb1e6bb34327bf9c4c
Levan <cstunao@gmail.com>
2019-12-12 12:15:48 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
+	// date
+	var $billboard = $('.billboard');
+	var date = new Date(), hour = date.getHours();
+	if (hour > 5 && hour < 18) {
+		$billboard.removeClass('dark').addClass('light');
+	} else {
+		$billboard.removeClass('light').addClass('dark');
+	}
 	//wow.js init
-	wow = new WOW(
-	    {
+	var wow = new WOW (
+	{
 		  animateClass: 'animated',
 		  mobile: false,
 		  offset: 100
